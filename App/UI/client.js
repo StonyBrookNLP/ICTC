defcff6119cad07b9febbe938c7725b43ea28bd4
Vijay Teja <gvteja@gmail.com>
2016-10-21 03:47:53 -0400

    Add suggestion box

--- App/UI/client.js
+++ App/UI/client.js
@@ -13,7 +13,7 @@ $(function () {
             }
         });
         e.preventDefault();
-    })
+    });
 
     $('#feedback_form').on('submit', function (e) {
         var feedback_data = {}
@@ -24,7 +24,9 @@ $(function () {
         var style_score = $("#feedback_form input[name=optionsStyle]:checked").val();
         feedback_data['content_score'] = parseInt(content_score, 10);
         feedback_data['style_score'] = parseInt(style_score, 10);
-
+        var suggestion_text = $("#suggestion_text").val();
+        if (suggestion_text)
+            feedback_data['suggestion_text'] = suggestion_text;
         $.ajax({
             type: "POST",
             url: $(this).attr('action'),
@@ -38,5 +40,23 @@ $(function () {
             }
         });
         e.preventDefault();
-    })
+    });
+
+    var checkLowScore = function() {
+        var content_score = $("#feedback_form input[name=optionsContent]:checked").val();
+        var style_score = $("#feedback_form input[name=optionsStyle]:checked").val();
+        content_score = parseInt(content_score, 10);
+        style_score = parseInt(style_score, 10);
+
+        if (content_score < 3 || style_score < 3) {
+            $("#suggestion_wrapper").show();
+        } else {
+            $("#suggestion_wrapper").hide();
+        }
+    }
+
+    $("#feedback_form input[name=optionsContent]").on('change', checkLowScore);
+
+    $("#feedback_form input[name=optionsStyle]").on('change', checkLowScore);
+
 });
